3ec4cf3976675edae5b330a9970668e4ccc4fca6
Sergii <sergii.khylko@outlook.com>
2023-03-22 16:04:45 +0200

    #2 refactor App component to use custom http hook

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 import Products from "./components/Products/Products";
 import NewProduct from "./components/NewProduct/NewProduct";
 import useHttp from "./hooks/use-http";
 
+
 function App() {
 
+  console.log("App")
   const [products, setProducts] = useState([]);
+  const {isLoading, error, sendHttpRequest: fetchProducts} = useHttp();
 
-  const manageProducts = productsData => {
-    const loadedProducts = [];
-    for (const productKey in productsData) {
-      loadedProducts.push({ id: productKey, text: productsData[productKey].text });
-    }
-    setProducts(loadedProducts);
-  }
-
-  const {isLoading, error, sendHttpRequest: fetchProducts} = useHttp({
-    endpoint: "https://react-http-custom-hook-10a71-default-rtdb.firebaseio.com/products.json",
-    manageProducts
-  });
 
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    console.log("fetchProducts in useEffect");
+
+    const manageProducts = productsData => {
+      console.log("manageProducts")
+      const loadedProducts = [];
+      for (const productKey in productsData) {
+        loadedProducts.push({ id: productKey, text: productsData[productKey].text });
+      }
+      setProducts(loadedProducts);
+    };
+
+    fetchProducts({
+      endpoint: "https://react-http-custom-hook-10a71-default-rtdb.firebaseio.com/products.json"
+    }, manageProducts);
+  }, [fetchProducts]);
 
   const productAddHandler = (product) => {
     setProducts((prevProducts) => prevProducts.concat(product));
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,14 +1,19 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
-const useHttp = (requestOptions, manageData) => {
+const useHttp = () => {
 
+  console.log("useHttp");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendHttpRequest = async (productText) => {
+  const sendHttpRequest = useCallback(async (requestOptions, manageData) => {
+    console.log("sendHttpRequest");
+    console.log("setIsLoading");
     setIsLoading(true);
+    console.log("setError");
     setError(null);
     try {
+      console.log("fetch");
       const response = await fetch(
         requestOptions.endpoint, {
           method: requestOptions.method ? requestOptions.method : "GET",
@@ -16,18 +21,22 @@ const useHttp = (requestOptions, manageData) => {
           body: requestOptions.body ? JSON.stringify(requestOptions.body) : null
         }
       );
+      console.log("fetch is done");
 
       if (!response.ok) {
         throw new Error("Request error.");
       }
-
+      console.log("response.json()");
       const data = await response.json();
+      console.log("manageData(data);");
       manageData(data);
     } catch (err) {
       setError(err.message || "Something went wrong");
     }
+    console.log("setIsLoading(false)");
     setIsLoading(false);
-  };
+  }, []);
+
   return {
     isLoading,
     error,
